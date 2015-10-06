b341748d634a81fff7b5a2a1c373152092c1e0a3
Axel Bellec <axel.bellec@outlook.fr>
2015-10-06 22:37:39 +0200

    modification script

--- kmeans.py
+++ kmeans.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from ggplot import *
-from sklearn.cluster import MiniBatchKMeans, KMeans
-from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sklearn.cluster import KMeans
 from sklearn.datasets.samples_generator import make_blobs
 
 ## Génération d'un jeu de données
@@ -16,14 +14,14 @@ X, labels_true = make_blobs(n_samples=3000, centers=centers, cluster_std=0.7)
 # labels_true correspond à l'assignation des points aux labels
 # c-à-d à quel cluster les points doivent appartenir
 
-# Lancement d'un méthode de clustering avec les kmeans
+## Lancement d'un méthode de clustering avec les kmeans
 k_means = KMeans(init='k-means++', n_clusters=3, n_init=10)
 k_means.fit(X)
 k_means_labels = k_means.labels_
 k_means_cluster_centers = k_means.cluster_centers_
 k_means_labels_unique = np.unique(k_means_labels)
 
-# Affichage du résultat avec plt
+## Affichage du résultat avec matplotlib
 fig = plt.figure(1, figsize=(8, 3))
 fig.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.9)
 colors = ['#F92672', '#66D9EF', '#A6E22E']
@@ -42,5 +40,4 @@ ax.set_xticks(())
 ax.set_yticks(())
 # Affichage de l'inertie
 plt.text(-3.5, 1.8, "Inertie : %.3f"%(k_means.inertia_))
-
-plt.show()
+plt.show() # Output du graphique
